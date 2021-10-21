f1fb4e4076405d312e34f2cf72783e47466b1926
Burak-Baylan <dsjkadnas@gmail.com>
2021-10-22 02:05:57 +0300

    add: google ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:human_benchmark/pages/home_page.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:human_benchmark/ads/controller/ads_controller.dart';
+import 'package:human_benchmark/pages/home/home_page.dart';
 import 'helpers/colors.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    Get.put(AdsController());
     return GetMaterialApp(
       theme: ThemeData(
         primaryColor: MyColors.myBlue,
