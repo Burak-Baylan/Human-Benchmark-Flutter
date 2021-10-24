777c72e269740420d3c1b34eee5bda5f125b579e
Burak-Baylan <dsjkadnas@gmail.com>
2021-10-24 23:51:58 +0300

    update: ads_controller

--- lib/ads/controller/ads_controller.dart
+++ lib/ads/controller/ads_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:human_benchmark/helpers/colorful_print.dart';
 
 class AdsController extends GetxController {
   var homeBanner = BannerAd(
@@ -9,4 +10,26 @@ class AdsController extends GetxController {
     request: AdRequest(),
   ).obs;
   var homeBannerAdIsReady = false.obs;
+
+  showAd(Object ad) {
+    if (ad is InterstitialAd) {
+      ad.show();
+    }else{
+      ColorfulPrint.red("AD TYPE ERROR.\nTYPE: ${ad.runtimeType}");
+    }
+  }
+
+  var sequenceMemoryInterstitialIsReady = false.obs;
+  var sequenceMemoryInterstitialAd = Object().obs;
+  lockSeqMemoryWrongAnswerInterstitial() =>
+      sequenceMemoryInterstitialIsReady.value = false;
+  unlockSeqMemoryWrongAnswerInterstitial() =>
+      sequenceMemoryInterstitialIsReady.value = true;
+
+  var numMemoryInterstitialIsReady = false.obs;
+  var numMemoryInterstitialAd = Object().obs;
+  lockNumMemoryWrongAnswerInterstitial() =>
+      sequenceMemoryInterstitialIsReady.value = false;
+  unlockNumMemoryWrongAnswerInterstitial() =>
+      sequenceMemoryInterstitialIsReady.value = true;
 }
